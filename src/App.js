1ca923296078a92872e4ce6e58e5bfb2357dff2f
crackingdemon <souravchauhanrajput@gmail.com>
2021-07-09 15:39:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import Header from "./containers/Header";
+import "./App.css";
+import ProductDetails from "./containers/ProductDetail";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" component={ProductDetails} />
+          <Route>404 Not Found!</Route>
+        </Switch>
+        <Footer/>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./index.css";
+const Footer =() => {
+    return(
+        <>
+       <footer className="footer">
+    <p>MadeBy Demon!!</p>
+  </footer>
+        </>
+    )
+};
+export default Footer;
